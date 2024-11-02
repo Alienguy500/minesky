db18343cd90f70d4fedcf877c17995b60ff70dd2
Alienguy500 <alienguy500@gmail.com>
2024-11-02 14:20:57 +0000

    Update Minesky.java

--- src/main/java/net/minesky/Minesky.java
+++ src/main/java/net/minesky/Minesky.java
@@ -2,8 +2,20 @@ package net.minesky;
 
 import net.fabricmc.api.ModInitializer;
 
+import static net.minecraft.server.command.CommandManager.*;
+
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+
 public class Minesky implements ModInitializer {
+    @Override
     public void onInitialize() {
-        
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
+        .executes(context -> {
+      // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+      // For versions below 1.20, remode "() ->" directly.
+      context.getSource().sendFeedback(() -> Text.literal("Called /foo with no arguments"), false);
+ 
+      return 1;
+    })));
     }
 }
